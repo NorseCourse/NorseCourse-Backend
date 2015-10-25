a698a89905a88efb93f69f7f0d3c4b3238d637c6
Blaise Schaeffer <schabl01@luther.edu>
2015-10-25 16:57:31 -0500

    Beginning basic implementation of APIs

--- database/divisionsAPI.py
+++ database/divisionsAPI.py
@@ -1,1 +1,20 @@
 # Import the needed packages
+import mysql.connector
+import mysql.connector.pooling
+
+import config
+
+db_properties = config.db_pool_config
+cnx_pool = mysql.connector.pooling.MySQLConnectionPool(**db_properties)
+
+cnx = cnx_pool.get_connection()
+cursor = cnx.cursor()
+
+termQuery = "SELECT DISTINCT name FROM Divisions"
+cursor.execute(termQuery)
+
+for name in cursor:
+	print(name[0])
+
+cursor.close()
+cnx.close()
--- database/termsAPI.py
+++ database/termsAPI.py
@@ -1,6 +1,20 @@
 # Import the needed packages
 import mysql.connector
 import mysql.connector.pooling
+
 import config
 
-print("Done")
+db_properties = config.db_pool_config
+cnx_pool = mysql.connector.pooling.MySQLConnectionPool(**db_properties)
+
+cnx = cnx_pool.get_connection()
+cursor = cnx.cursor()
+
+termQuery = "SELECT DISTINCT term FROM Sections"
+cursor.execute(termQuery)
+
+for term in cursor:
+	print(term[0])
+
+cursor.close()
+cnx.close()

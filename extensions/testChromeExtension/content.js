c825b6fd19db045ded939e20242ed17105677520
Blaise Schaeffer <schabl01@luther.edu>
2016-03-08 14:03:14 -0600

    extensions

--- extensions/testChromeExtension/content.js
+++ extensions/testChromeExtension/content.js
@@ -3,14 +3,19 @@
 // var firstHref = $("a[href^='http']").eq(0).attr("href");
 // console.log(firstHref);
 
+// console.log("Hello, world!");
+// angular.module("norseCourse").service("schedulesService").getSavedSchedules();
+
 chrome.runtime.onMessage.addListener(
   function(request, sender, sendResponse) {
     if( request.message === "clicked_browser_action" ) {
       var firstHref = $("a[href^='http']").eq(0).attr("href");
-
-      console.log(firstHref);
-
+      // console.log(firstHref);
       chrome.runtime.sendMessage({"message": "open_new_tab", "url": firstHref});
+
+      // var testing = $("md-list-item").get();
+      // console.log(testing);
+
     }
   }
 );

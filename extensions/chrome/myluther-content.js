1a28b874d670227a3700b81aa34bf92ab7a9acf7
Blaise Schaeffer <schabl01@luther.edu>
2016-03-29 15:31:54 -0500

    Minor extension changes

--- extensions/chrome/myluther-content.js
+++ extensions/chrome/myluther-content.js
@@ -1,3 +1,9 @@
+// my.luther form id's 
+var subjectBox = "#LIST_VAR1_";
+var courseNumberBox = "#LIST_VAR3_";
+var sectionNumberBox = "#LIST_VAR4_";
+
+
 // Listen for message to register schedule
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 	if (request.message === "register_schedule") {
@@ -8,21 +14,40 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
 			var sectionNumbers = scheduleData.sectionNumbers;
 			var numberOfCourses = departments.length;
 
-			// Set the term value to the most recent term, this will wither be spring or fall
-			$("#VAR1").val($("#VAR1 option:nth-child(2)").val());
+			if (departments == "") {
+				alert("No saved schedules have been retrieved.")
+			} else {
+				// Clear the form if there is anything there
+				clearMyLutherForm();
 
-			var subjectBox = "#LIST_VAR1_";
-			var courseNumberBox = "#LIST_VAR3_";
-			var sectionNumberBox = "#LIST_VAR4_";
+				// Set the term value to the most recent term, this will wither be spring or fall
+				$("#VAR1").val($("#VAR1 option:nth-child(2)").val());
 
-			// Populate the form on my.luther with the saved schedule 
-			for (var i = 1; i <= numberOfCourses; i++) {
-				var iStr = i.toString();
-				var idx = i - 1;
-				$(subjectBox + iStr).val(departments[idx]);
-				$(courseNumberBox + iStr).val(courseNumbers[idx]);
-				$(sectionNumberBox + iStr).val(sectionNumbers[idx]);
+				// Populate the form on my.luther with the saved schedule 
+				for (var i = 1; i <= numberOfCourses; i++) {
+					var iStr = i.toString();
+					var idx = i - 1;
+					$(subjectBox + iStr).val(departments[idx]);
+					$(courseNumberBox + iStr).val(courseNumbers[idx]);
+					$(sectionNumberBox + iStr).val(sectionNumbers[idx]);
+				}
 			}
 		});
 	}
 });
+
+
+function clearMyLutherForm() {
+	// Set the term value as empty
+	$("#VAR1").val($("#VAR1 option:nth-child(1)").val());
+
+	for (var i = 1; i <= 5; i++) {
+		var iStr = i.toString();
+
+		var clearSubject = subjectBox + iStr;
+
+		$(clearSubject).val($(clearSubject + " option:nth-child(1)").val());
+		$(courseNumberBox + iStr).val("");
+		$(sectionNumberBox + iStr).val("");
+	}
+}
